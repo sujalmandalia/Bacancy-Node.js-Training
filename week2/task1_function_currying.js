f07009d66bde14827a2ff5417a5ba7f2c8fd5c18
Sujal Mandalia <82870049+SUJAL-MANDALIA@users.noreply.github.com>
2024-01-23 10:51:51 +0530

    Adds : task1_function_currying

---
+++ week2/task1_function_currying.js
@@ -0,0 +1,24 @@
+// using Bind method
+let multipy = function(x, y) {
+  return x * y;
+};
+
+//1st way
+// let multiplyByTwo = multipy.bind(this, 2, 3);// it will take x = 2 and y = 3
+// console.log(multiplyByTwo());
+
+// 2nd Way
+let multiplyByTwo = multipy.bind(this, 2); //x = 2
+console.log(multiplyByTwo(3)); // y= 3
+
+
+//Using Function Closures
+
+function mul(x, y){
+  return function(y){
+    return x * y;
+  }
+}
+
+let multiplyByFive = mul(5);
+console.log(multiplyByFive(10));

c30497f029218bb2c4b0ec4bd28887d8f564aa48
Sujal Mandalia <82870049+SUJAL-MANDALIA@users.noreply.github.com>
2024-01-30 13:22:05 +0530

    updates : task7_modified

--- week3/task7_cli.js
+++ week3/task7_cli.js
@@ -1,23 +1,26 @@
+/* eslint-disable indent */
 const args = process.argv.slice(2);
 // console.log(args);
 let result;
-
-switch (args[2]) {
-case '+':
-  result = Number(args[0]) + Number(args[1]);
-  break;
-case '-':
-  result = Number(args[0]) - Number(args[1]);
-  break;
-case '*':
-  result = Number(args[0]) * Number(args[1]);
-  break;
-case '/':
-  result = Number(args[0]) / Number(args[1]);
-  break;
-default:
-  result = 'Invalid Operator';
-  break;
+if (isNaN(args[0]) || isNaN(args[1])) {
+  result = 'Enter Valid Operand';
+} else {
+  switch (args[2]) {
+    case '+':
+      result = Number(args[0]) + Number(args[1]);
+      break;
+    case '-':
+      result = Number(args[0]) - Number(args[1]);
+      break;
+    case '*':
+      result = Number(args[0]) * Number(args[1]);
+      break;
+    case '/':
+      result = Number(args[0]) / Number(args[1]);
+      break;
+    default:
+      result = 'Invalid Operator';
+      break;
+  }
 }
-
 console.log(result);
